a79a816414b73099c9cbbc415a13a3a3410e515a
Yeison Mestra <ymmestram@ufpso.edu.co>
2023-11-30 16:02:44 -0500

    listo

--- back/middlewares/index.middleware.js
+++ back/middlewares/index.middleware.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { verifyToken } from "./token.middleware.js";
-
+import cors from 'cors'
 
 const router = Router();
+
+app.use(cors({
+    origin: 'https://angular-front-d2426.web.app', // Permitir solicitudes solo desde esta URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Habilitar el envío de cookies de autenticación
+  }));
+  
 
 router.use("/producto", verifyToken);
 export default router;
