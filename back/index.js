f37d9f6ce30f0ce2519abe1aeb7c63c09237fca6
Yeison Mestra <ymmestram@ufpso.edu.co>
2023-11-30 00:09:40 -0500

    listo

--- back/index.js
+++ back/index.js
@@ -1,9 +1,3 @@
 import Server from "./config/server.js"
-const cors = require('cors');
 const server = new Server();
-app.use(cors({
-    origin: 'http://localhost:4200',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  }));
 server.load();
