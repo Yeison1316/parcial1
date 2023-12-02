c427a143832503eb4f29d94fc943ff3054372305
Yeison Mestra <ymmestram@ufpso.edu.co>
2023-12-02 12:53:57 -0500

    listo

--- back/controllers/category.controllers.js
+++ back/controllers/category.controllers.js
@@ -5,7 +5,6 @@ export const getCategoryALL = async (req,res) => {
       let data = await getCategory();
       res.send({ success: true, msg: "HOLA GET CATEGORY", data: data });
     } catch (error) {
-      console.log(error);
       res.status(500).json({ success: false, msg: "Error al obtener las categorias" });
     }
   };
--- back/models/category.models.js
+++ back/models/category.models.js
@@ -1,6 +1,10 @@
 import pgServices from "../services/pg.services.js";
 
 export const getCategory = async () => {
-    let con = new pgServices();
-    return await con.connection.query(`SELECT * FROM categories`);
-}
+    try {
+      let con = new pgServices();
+      return await con.connection.query(`SELECT * FROM categories`);
+    } catch (error) {
+      throw new Error("Error en getCategory: " + error.message);
+    }
+  };
