80abf002023f0d2dad970b4e6451e1f6d4ad5276
Yeison Mestra <ymmestram@ufpso.edu.co>
2023-12-02 11:45:48 -0500

    listo

--- back/models/category.models.js
+++ back/models/category.models.js
@@ -1,6 +1,6 @@
 import pgServices from "../services/pg.services.js";
 
-export const getCategory = async (category_id) => {
+export const getCategory = async () => {
     let con = new pgServices();
     return await con.connection.query(`SELECT * FROM categories`);
 }
