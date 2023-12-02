9b06889bc2acfe40950fe36dddedfdacc04fd9c2
Yeison Mestra <ymmestram@ufpso.edu.co>
2023-12-02 12:52:00 -0500

    listo

--- back/controllers/category.controllers.js
+++ back/controllers/category.controllers.js
@@ -1,10 +1,11 @@
 import { getCategory } from "../models/category.models.js";
 
-export const getCategoryALL = async (res) => {
+export const getCategoryALL = async (req,res) => {
     try {
       let data = await getCategory();
       res.send({ success: true, msg: "HOLA GET CATEGORY", data: data });
     } catch (error) {
+      console.log(error);
       res.status(500).json({ success: false, msg: "Error al obtener las categorias" });
     }
   };
